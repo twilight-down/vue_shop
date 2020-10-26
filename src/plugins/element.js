1107c8597f42d946ae2d57109b50c2e4852524c3
twilight-down <www.12550.com@qq.com>
2020-10-26 20:44:10 +0800

    完成了登录退出功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Form, FormItem, Button, Input, Message } from 'element-ui'
+
 
 Vue.use(Button)
+Vue.use(Input)
+Vue.use(Form)
+Vue.use(FormItem)
+
+//将弹框组件挂载到了原型组件身上
+Vue.prototype.$message = Message
