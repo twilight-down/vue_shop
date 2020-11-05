799982636e922bc3226e246ce117124bfb65d830
twilight-down <www.12550.com@qq.com>
2020-11-05 14:50:16 +0800

    完成了订单开发功能

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import 'quill/dist/quill.bubble.css' // for bubble theme
 import axios from 'axios'
 
 // 配置请求的跟路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://47.92.6.241:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
     // console.log(config);
     config.headers.Authorization = window.sessionStorage.getItem('token')
